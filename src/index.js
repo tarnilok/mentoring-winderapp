5d019b85a79496ceecaa3b404838331ad71ec6b2
tarnilok <tarnilok@gmail.com>
2022-04-18 16:57:04 +0300

    index.js tidying

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
 
-
-// ReactDOM.render(
-//     <App />, document.getElementById('root')
-// );
-
-// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
-//   <StrictMode>
     <App />
-//   </StrictMode>
 );
 
